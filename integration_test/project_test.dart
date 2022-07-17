30b9dddc96d690d54f1e15623a6a2a2699d5c24a
hbedford <36144175+hbedford@users.noreply.github.com>
2022-07-17 01:37:25 -0300

    add corrections

--- integration_test/project_test.dart
+++ integration_test/project_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:project_tests/src/app.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets("Testando o projeto", (WidgetTester tester) async {
     await tester.pumpWidget(const App());
     await tester.pumpAndSettle();
