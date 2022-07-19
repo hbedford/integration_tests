93b6fce01c208fe416d986363174758710d91d74
hbedford <36144175+hbedford@users.noreply.github.com>
2022-07-19 02:15:26 -0300

    add corrections

---
+++ appclient/integration_test/screen_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:project_tests/app.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets("Testando o projeto", (WidgetTester tester) async {
+    await tester.pumpWidget(const App());
+    await tester.pumpAndSettle();
+    expect(find.text("Ola"), findsOneWidget);
+  });
+  testWidgets("Testando o projeto1", (WidgetTester tester) async {
+    await tester.pumpWidget(const App());
+    await tester.pumpAndSettle();
+    expect(find.text("Olaa"), findsNothing);
+  });
+}
