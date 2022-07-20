eb146f1dc6261eff5bd2db965486480b1af8bd8e
hbedford <36144175+hbedford@users.noreply.github.com>
2022-07-20 11:38:28 -0300

    add tests

--- appclient/integration_test/screen_test.dart
+++ appclient/integration_test/screen_test.dart
@@ -1,16 +1,21 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:project_tests/app.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  makeTestableWidget() => const MaterialApp(
+        home: App(),
+      );
   testWidgets("Testando o projeto", (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(makeTestableWidget());
     await tester.pumpAndSettle();
     expect(find.text("Ola"), findsOneWidget);
   });
   testWidgets("Testando o projeto1", (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(makeTestableWidget());
     await tester.pumpAndSettle();
     expect(find.text("Olaa"), findsNothing);
   });
